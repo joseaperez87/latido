3347545d2e5fef0e65166ab03e404f6058ca115e
joseaperez87 <43796082+joseaperez87@users.noreply.github.com>
2022-08-05 13:18:28 +0300

    Add Bootstrap, Vue and sass

---
+++ resources/js/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <font-awesome-icon icon="fa-solid fa-user-secret"></font-awesome-icon>
+        Hello!! How To Install Vue 3 in Laravel 9 with Vite - TechvBlogs
+    </div>
+</template>
+
+<script>
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+library.add(faUserSecret);
+export default {
+    name: "App"
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-import './bootstrap';
+// app.js
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/sass/styles.sass'
+
+import {createApp} from 'vue'
+
+import App from './App.vue' 
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount("#app")
+import 'bootstrap/dist/js/bootstrap.min.js'
